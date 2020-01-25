3386090f2be4c8a6e09e7256d021298c2c1e1e6e
joel sithivong <joelsithivong@MacBook-Pro-de-joel.local>
2020-01-25 19:32:02 +0100

    Updates

---
+++ precache-manifest.8976398defbe829d0ea3c7e73420e8d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb3b44e4aa3b891ad1dab1bafdae7e4",
+    "url": "/markdown/index.html"
+  },
+  {
+    "revision": "82c00c1861eb607ebe7f",
+    "url": "/markdown/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3104d55213bc3e260816",
+    "url": "/markdown/static/css/main.14f218a4.chunk.css"
+  },
+  {
+    "revision": "82c00c1861eb607ebe7f",
+    "url": "/markdown/static/js/2.6c5965cc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/markdown/static/js/2.6c5965cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3104d55213bc3e260816",
+    "url": "/markdown/static/js/main.0bc924d8.chunk.js"
+  },
+  {
+    "revision": "28fafea90d45b7ee5918",
+    "url": "/markdown/static/js/runtime-main.caeef3b1.js"
+  }
+]);
