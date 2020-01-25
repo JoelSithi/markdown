975879e8a31e86f2f76936fe955c627e442099bc
joel sithivong <joelsithivong@MacBook-Pro-de-joel.local>
2020-01-25 19:26:18 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import marked from 'marked'
+import { sampleText } from './sampleText'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  /*Creating State*/
+  state = {
+    text: sampleText
+  }
+  
+  componentDidMount () {
+    const text = localStorage.getItem('text')
+
+     if (text) {
+      this.setState({ text })
+     } else {
+       this.setState({ text: sampleText })
+     }
+  }
+  
+  /*Saving state with local storage*/
+  componentDidUpdate () {
+    const { text } = this.state
+    localStorage.setItem('text', text)
+  }
+ 
+  /*Handling events in textarea*/
+  handleChange = event => {
+      const text = event.target.value
+     this.setState({text})
+  }
+
+  renderText = text => {
+    const __html = marked(text, { sanitize: true})
+    return { __html} // ou return { __html: __html}
+  }
+
+  render () { 
+    return (
+      <div className="container">
+        <div className='row'>
+          <div className='col-sm-6'>
+            <textarea
+             onChange = { this.handleChange}
+             /*State managing the textarea*/
+             value={ this.state.text } 
+             className='form-control' 
+             row='35' />
+          </div>
+          <div className="col-sm-6">
+            <div dangerouslySetInnerHTML={this.renderText(this.state.text)}></div>
+          </div>
+        </div>
+      </div>  
+    )
+  }
 }
 
-export default App;
+export default App
